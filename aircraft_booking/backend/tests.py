83b5d60ef026ee921d276b60f748556ff0af4bea
Jakub Swistak <jakub.swistak@gmail.com>
2023-03-18 17:49:12 +0100

    User endpoint tests

--- aircraft_booking/backend/tests.py
+++ aircraft_booking/backend/tests.py
@@ -1,3 +1,37 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
+from django.contrib.auth.models import User
+from rest_framework.test import APIRequestFactory
+from .views import UserViewSet, GroupViewSet, AircraftViewSet
 
-# Create your tests here.
+
+class UserTestCase(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        User.objects.create_user("jakub", "jakub@email.com", "password")
+
+    def test_user_exists(self):
+        request = self.factory.get('/users/')
+        response = UserViewSet.as_view({'get': 'list'})(request)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(User.objects.count(), 1)
+
+    def test_user_has_username(self):
+        request = self.factory.get('/users/')
+        response = UserViewSet.as_view({'get': 'list'})(request)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(User.objects.count(), 1)
+        self.assertEqual(response.data[0]['username'], 'jakub')
+
+    def test_user_has_email(self):
+        request = self.factory.get('/users/')
+        response = UserViewSet.as_view({'get': 'list'})(request)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(User.objects.count(), 1)
+        self.assertEqual(response.data[0]['email'], 'jakub@email.com')
+
+    def test_user_has_no_password(self):
+        request = self.factory.get('/users/')
+        response = UserViewSet.as_view({'get': 'list'})(request)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(User.objects.count(), 1)
+        self.assertNotIn('password', response.data[0])
