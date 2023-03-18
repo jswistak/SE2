38fe48fa008fa353f251b76b643ee71f75e2d819
Jakub Swistak <jakub.swistak@gmail.com>
2023-03-18 16:20:31 +0100

    Add fix typo in Certificate

--- aircraft_booking/backend/models.py
+++ aircraft_booking/backend/models.py
@@ -17,4 +17,4 @@ class Aircraft(models.Model):
 class Certificate(models.Model):
     certificate_name = models.CharField(max_length=50, primary_key=True)
     def __str__(self):
-        return self.certificate_id
+        return self.certificate_name
