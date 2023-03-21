2d2babeb5786e4bb8a3a47baec7b49b9dafec16a
justynapokora <justynapokora@icloud.com>
2023-03-21 04:31:31 +0100

    add JWT login and authetication tests

--- aircraft_booking/backend/tests.py
+++ aircraft_booking/backend/tests.py
@@ -1,8 +1,14 @@
 from django.test import TestCase, RequestFactory
 from django.contrib.auth.models import User
-from rest_framework.test import APIRequestFactory
 from .views import UserViewSet, GroupViewSet, AircraftViewSet
-
+from rest_framework.test import APIClient
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenViewBase,
+    TokenRefreshView,
+    TokenVerifyView,
+)
 
 class UserTestCase(TestCase):
     def setUp(self):
@@ -35,3 +41,36 @@ class UserTestCase(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(User.objects.count(), 1)
         self.assertNotIn('password', response.data[0])
+
+
+class JWTLoginTestCase(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.client = APIClient()
+        self.user = User.objects.create_user("justyna", "justyna@email.com", "password123")
+        self.token = str(RefreshToken.for_user(self.user).access_token)
+
+    def test_login_success(self):
+        request_data = {'username': 'justyna', 'password': 'password123'}
+        request = self.factory.post('/api/login/', request_data)
+        response = TokenObtainPairView.as_view()(request)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('access', response.data)
+        self.assertIn('refresh', response.data)
+
+    def test_login_failure(self):
+        request_data = {'username': 'justyna', 'password': 'wrong_pass'}
+        request = self.factory.post('/api/login/', request_data)
+        response = TokenObtainPairView.as_view()(request)
+        self.assertEqual(response.status_code, 401)
+
+    def test_authenticate_success(self):
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + self.token)
+        response = self.client.get('/status/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {'status': 'OK'})
+
+    def test_authenticate_failure(self):
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + 'invalid_token')
+        response = self.client.get('/status/')
+        self.assertEqual(response.status_code, 401)
