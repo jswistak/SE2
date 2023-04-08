b83044e34866e5c2864007aa215e874d09f98627
Jakub Swistak <jakub.swistak@gmail.com>
2023-04-08 22:01:37 +0200

    update aircraft endpoint

--- aircraft_booking/backend/views.py
+++ aircraft_booking/backend/views.py
@@ -37,6 +37,15 @@ class AircraftViewSet(viewsets.ModelViewSet):
     queryset = Aircraft.objects.all()
     serializer_class = AircraftSerializer
 
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        return JsonResponse(serializer.data)
+
     # Required for authentication
     permission_classes = [IsAuthenticated]
 
