e01a7a9c8313ea61e5dd6269fba939c192348e22
DanielM32 <125911410+DanielM32@users.noreply.github.com>
2023-05-23 00:05:51 +0200

    Calendar
    2 Calendar views

---
+++ web_front/src/pages/Calendar.js
@@ -0,0 +1,89 @@
+// Calendar.js
+
+import React, { useState } from 'react';
+import styles from './Calendar.module.css';
+import "./App.css";
+
+const Calendar = () => {
+    const currentDate = new Date();
+    const [currentMonth, setCurrentMonth] = useState(currentDate.getMonth());
+    const [currentYear, setCurrentYear] = useState(currentDate.getFullYear());
+    const daysInMonth = new Date(currentYear, currentMonth + 1, 0).getDate();
+
+    const [bookedDays, setBookedDays] = useState({});
+
+    const monthNames = [
+        'January', 'February', 'March', 'April', 'May', 'June',
+        'July', 'August', 'September', 'October', 'November', 'December'
+    ];
+
+    const changeMonth = (delta) => {
+        let newMonth = currentMonth + delta;
+        let newYear = currentYear;
+
+        if (newMonth < 0) {
+            newMonth = 11;
+            newYear--;
+        } else if (newMonth > 11) {
+            newMonth = 0;
+            newYear++;
+        }
+
+
+
+        setCurrentMonth(newMonth);
+        setCurrentYear(newYear);
+
+
+    };
+
+    const bookDay = (day) => {
+        // TODO: Replace this with a call to your API endpoint for booking, and handle the response accordingly.
+        // Replace this with actual description of user and/or flight/aircraft
+        const eventTitle = 'Sample Event';
+        const eventDescription = 'This is a sample event description.';
+        setBookedDays({
+            ...bookedDays,
+            [day]: { title: eventTitle, description: eventDescription },
+        });
+    };
+
+    const days = [];
+    for (let i = 1; i <= daysInMonth; i++) {
+        const eventDetails = bookedDays[i];
+
+        days.push(
+            <div
+                key={i}
+                className={`${styles['calendar-day']} ${eventDetails ? styles.booked : ''}`}
+                onClick={() => {
+                    if (!eventDetails) {
+                        bookDay(i);
+                    }
+                }}
+            >
+                {i}
+                {eventDetails && (
+                    <div className={styles.tooltip}>
+                        <strong>{eventDetails.title}</strong>
+                        <br />
+                        {eventDetails.description}
+                    </div>
+                )}
+            </div>
+        );
+    }
+
+    return (
+        <div>
+            <div className={styles.navigation}>
+                <button onClick={() => changeMonth(-1)}>&lt;</button>
+                <span>{monthNames[currentMonth]} {currentYear}</span>
+                <button onClick={() => changeMonth(1)}>&gt;</button>
+            </div>
+            <div className={styles.calendar}>{days}</div>
+        </div>
+    );
+};
+
+export default Calendar;
---
+++ web_front/src/pages/WeekView.js
@@ -0,0 +1,38 @@
+// WeekView.js
+
+import React from 'react';
+import styles from './WeekView.module.css';
+import "./App.css";
+
+const WeekView = ({ currentMonth, currentYear }) => {
+    // Samples for demonstration:
+    const events = [
+        {
+            day: 1,
+            time: '09:00',
+            title: 'Sample Event 1',
+            description: 'This is a sample event description.',
+        },
+        {
+            day: 3,
+            time: '14:00',
+            title: 'Sample Event 2',
+            description: 'This is another sample event description.',
+        },
+    ];
+
+    return (
+        <div className={styles.week}>
+            {events.map((event, index) => (
+                <div key={index} className={styles.event}>
+                    <div className={styles.day}>Day {event.day}</div>
+                    <div className={styles.time}>{event.time}</div>
+                    <div className={styles.title}>{event.title}</div>
+                    <div className={styles.description}>{event.description}</div>
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default WeekView;
